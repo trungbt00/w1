d137583b533fc1cafcfd1271214700acc4d92ec2
trungbt00 <trungbt00@gmail.com>
2025-04-01 17:02:10 +0700

    Update News1.js

--- src/pages/News/News1.js
+++ src/pages/News/News1.js
@@ -12,7 +12,7 @@ const Article = () => {
             .then((data) => {
                 const formattedData = data.items.map((item) => ({
                     id: item.id,
-                    date: new Date(item.publishDate).toISOString().split("T")[0], // Format lại ngày
+                    date: new Date(item.publishDate).toISOString().split("T")[0], 
                     title: item.title,
                     image: item.imageRepresent,
                 }));
